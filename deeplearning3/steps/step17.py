5015a485ade8992b3fb8f1702c9210ae1c98174c
wshf253 <65922112+wshf253@users.noreply.github.com>
2023-12-09 00:29:41 +0900

    12/08 ~step18

---
+++ deeplearning3/steps/step17.py
@@ -0,0 +1,95 @@
+import numpy as np
+import weakref
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported'.format(type(data)))
+            
+        self.data = data
+        self.grad = None
+        self.creator = None
+        self.generation = 0
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
+    
+    def clear_grad(self):
+        self.grad = None
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+        
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop()
+            # before: gys = [output.grad for output in f.outputs]
+            gys = [output().grad for output in f.outputs] # because of weakref
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    add_func(x.creator)
+
+class Function:
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        self.generation = max([x.generation for x in inputs])
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = [weakref.ref(output) for output in outputs] # to prevent recurrent reference between func and outputs (by func.outputs, var.creator)
+        return outputs if len(outputs) > 1 else outputs[0]
+    
+    def foward(self, xs):
+        raise NotImplementedError()
+    
+    def backward(self, gys):
+        raise NotImplementedError()
+    
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+    
+    def bakcard(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+def square(x): 
+    return Square()(x)
+
+for i in range(10):
+    x = Variable(np.random.randn(10000))
+    y = square(square(square(x)))
