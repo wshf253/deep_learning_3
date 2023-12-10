99df643a65bc69f6bc333174a750c4031b67d0ec
wshf253 <65922112+wshf253@users.noreply.github.com>
2023-12-11 00:39:10 +0900

    12/10 ~step23

---
+++ dezero/__init__.py
@@ -0,0 +1,16 @@
+# True: ~step32
+is_simple_core = True
+
+if is_simple_core:
+    from dezero.core_simple import Variable
+    from dezero.core_simple import Function
+    from dezero.core_simple import using_config
+    from dezero.core_simple import no_grad
+    from dezero.core_simple import as_array
+    from dezero.core_simple import as_variable
+    from dezero.core_simple import setup_varibale
+else:
+    a = 1 
+    # add later
+
+setup_varibale()
