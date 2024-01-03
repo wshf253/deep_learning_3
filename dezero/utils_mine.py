278f81144c0d6688d74cd3133bd09a7fbec04813
wshf253 <65922112+wshf253@users.noreply.github.com>
2024-01-04 00:30:48 +0900

    1/3 ~step57
    conv2d review needed

--- dezero/utils_mine.py
+++ dezero/utils_mine.py
@@ -134,3 +134,11 @@ def logsumexp(x, axis=1):
     return m
 
 
+def pair(x):
+    if isinstance(x, int):
+        return (x, x)
+    elif isinstance(x, tuple):
+        assert len(x) == 2
+        return x
+    else:
+        raise ValueError
